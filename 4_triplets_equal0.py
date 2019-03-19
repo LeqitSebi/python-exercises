2d9a3825d090f71bb7719511822cf2debb4bf138
Sebastian Slanitsch <7225@htl.rennweg.at>
2019-03-19 17:20:51 +0100

    beginning 4. triplets_equal0 -> not finished 5. all_combos

---
+++ 4_triplets_equal0.py
@@ -0,0 +1,11 @@
+#not a finished programm
+numbers = [1, 2, 4, 5, -5, -4, -6, 7, -9]
+
+
+def find_triplets(list):
+    output = []
+    list.sort()
+
+
+#still trying to figure out a relatively good
+# algorythm to calculate all triples that equal 0
