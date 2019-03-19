818018590a2f8453bc38f3191a45cf6d51cc46a9
Sebastian Slanitsch <7225@htl.rennweg.at>
2019-03-19 11:15:49 +0100

    1. diff_nums

---
+++ python_exercices.py
@@ -0,0 +1,17 @@
+nums = [1,4,2,23,5, 5]
+
+def checknums(toBeChecked, checkList):
+    for number in checkList:
+        if number==toBeChecked:
+            return 0
+
+def diff_nums(numbers):
+    newlist = []
+    for number in nums:
+        if checknums(number, newlist)==0:
+            return "es gibt doppelte Zahlen"
+        else:
+            newlist.append(number)
+    return "es gibt keine doppelten Zahlen"
+
+print(diff_nums(nums))
